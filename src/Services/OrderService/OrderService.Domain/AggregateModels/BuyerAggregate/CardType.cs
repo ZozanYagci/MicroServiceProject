aad00aac35a4a2666193a36139c8fd8713450e22
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-04-18 15:16:07 +0300

    add domain layer for Order Service

---
+++ src/Services/OrderService/OrderService.Domain/AggregateModels/BuyerAggregate/CardType.cs
@@ -0,0 +1,21 @@
+﻿using OrderService.Domain.SeedWork;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Domain.AggregateModels.BuyerAggregate
+{
+    public class CardType : Enumeration
+    {
+        public static CardType Anex = new(1, nameof(Anex));
+        public static CardType Visa = new(2, nameof(Visa));
+        public static CardType MasterCard = new(3, nameof(MasterCard));
+
+        public CardType(int id, string name) : base(id, name)
+        {
+
+        }
+    }
+}
