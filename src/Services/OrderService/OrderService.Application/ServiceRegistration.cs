b48151b4b880d4f7185611819ef85c6a7a281d56
ZozanYagci <166850421+ZozanYagci@users.noreply.github.com>
2025-04-24 22:12:41 +0300

    add application layer for order service

---
+++ src/Services/OrderService/OrderService.Application/ServiceRegistration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using OrderService.Application.Features.Commands.CreateOrder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderService.Application
+{
+    public static class ServiceRegistration
+    {
+        public static IServiceCollection AddApplicationRegistration(this IServiceCollection services, Type type)
+        {
+
+            var assm = Assembly.GetExecutingAssembly();
+
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assm));
+            services.AddAutoMapper(assm);
+
+            return services;
+
+        }
+    }
+}
